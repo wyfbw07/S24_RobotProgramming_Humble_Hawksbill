c2587054ef581b99fd6eefc17e071aea0aa177bb
Antonio Martinez III <toniom4172@gmail.com>
2024-04-22 15:12:16 -0400

    s_m sub

---
+++ FinalProject/roscourse_final_ws (copy)/src/pubsub/pubsub/square_movement_sub.py
@@ -0,0 +1,36 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import math
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            Twist,
+            '/cmd_vel',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('Received: "v_x: %f, v_y: %f, v_z: %f"' % (msg.linear.x, msg.linear.y, msg.linear.z))
+        self.get_logger().info('Received: "v_x: %f, v_y: %f, v_z: %f"' % (msg.angular.x, msg.angular.y, msg.angular.z))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_subscriber = MinimalSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
