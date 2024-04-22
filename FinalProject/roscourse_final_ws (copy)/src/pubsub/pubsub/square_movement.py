7000ed85a2fb407027f5298c32ee5d29781acef4
alessiocalcagni <156835948+alessiocalcagni@users.noreply.github.com>
2024-04-22 16:26:27 -0400

    Update square_movement.py

--- FinalProject/roscourse_final_ws (copy)/src/pubsub/pubsub/square_movement.py
+++ FinalProject/roscourse_final_ws (copy)/src/pubsub/pubsub/square_movement.py
@@ -11,7 +11,7 @@ class SquareMovement(Node):
         timer_period = 1.0  # seconds
         self.timer = self.create_timer(timer_period, self.move_robot)
         self.linear_speed = 0.2  # m/s
-        self.angular_speed = 0.14  # rad/s
+        self.angular_speed = 0.1 # rad/s
         self.side_length = 0.5  # meters
         self.current_angle = 0.0  # radians
         self.current_side = 0
