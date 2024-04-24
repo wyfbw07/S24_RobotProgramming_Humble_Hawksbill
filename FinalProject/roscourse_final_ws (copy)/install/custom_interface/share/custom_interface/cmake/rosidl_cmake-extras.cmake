0779bdb7613def59d641cf6eb90703d205bf06e2
Yifan Wang <wyfbw07@gmail.com>
2024-04-23 20:15:04 -0400

    Remove install folder

--- FinalProject/roscourse_final_ws (copy)/install/custom_interface/share/custom_interface/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(custom_interface_IDL_FILES "srv/SetVelocity.idl")
-set(custom_interface_INTERFACE_FILES "srv/SetVelocity.srv;srv/SetVelocity_Request.msg;srv/SetVelocity_Response.msg")
