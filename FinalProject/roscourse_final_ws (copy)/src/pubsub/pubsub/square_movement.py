0ff766bfb0f02de50af0505cbcb7702a3d8edf4f
Antonio Martinez III <73197290+xDiggy@users.noreply.github.com>
2024-04-24 17:29:16 -0400

    debug print

--- FinalProject/roscourse_final_ws (copy)/src/pubsub/pubsub/square_movement.py
+++ FinalProject/roscourse_final_ws (copy)/src/pubsub/pubsub/square_movement.py
@@ -43,7 +43,7 @@ class SquareMovement(Node):
 
         speech_r = spe.speech_read()
 		#speech
-        #print(speech_r)
+        print("recieved", speech_r)
         if speech_r == 4:
             spe.void_write(speech_r)
             # Move forward
