73bb535cd2e716ee23f85f3af528054da7593329
Yifan Wang <wyfbw07@gmail.com>
2024-04-23 20:47:22 -0400

    Fix indentation

--- FinalProject/roscourse_final_ws (copy)/src/pubsub/pubsub/square_movement.py
+++ FinalProject/roscourse_final_ws (copy)/src/pubsub/pubsub/square_movement.py
@@ -19,11 +19,11 @@ class SquareMovement(Node):
         self.velocity_srv = self.create_service(SetVelocity, 'setVelocity', self.set_velocity_callback)
         
     def set_velocity_callback(self,request,response):
-	self.linear_speed = request.velocity
-	self.get_logger().info('Velocity set: %s' % (self.velocity))
-	response.ret = 1
-	return response
-
+        self.linear_speed = request.velocity
+        self.get_logger().info('Velocity set: %s' % (self.velocity))
+        response.ret = 1
+        return response
+    
     def move_robot(self):
         twist = Twist()
 
